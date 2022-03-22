216024375e33b2f291583dd685140c17f9a810d7
Rajan Agarwal <rajan.ag005@gmail.com>
2022-03-21 21:16:42 -0400

    update coloring system

--- pages/index.js
+++ pages/index.js
@@ -75,23 +75,26 @@ export async function getStaticProps() {
 }
 
 
-function Product({ name, about, status, category }) {
+function Product({ name, about, status, category, link }) {
     function Colours() {
-        if(status == 'Open') {
+        if(status == 'Case Study') {
             return '#08b445'
         }
-        if (status == 'Soon') {
+        if(status == 'Sample') {
+          return '#AA336A'
+      }
+        if (status == 'Terms') {
             return '#c89809'
         }
-        if (status == 'Closed') {
-            return 'red'
+        if (status == 'Notes') {
+            return '#BF40BF'
         }
     }
 
     return (
     <div className="max-w-sm rounded-lg border m-3 shadow-md bg-gray-300 border-gray-300">
     <div className="p-5">
-        <a href="#"
+        <a href={link}
         style={{
           display: 'flex',
         }}
@@ -111,7 +114,7 @@ function Product({ name, about, status, category }) {
 
         <p className="mb-3 font-normal text-gray-700"><i>{category}</i></p>
         <p className="mb-3 font-normal text-gray-700">{about}</p>
-        <a href="#" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white rounded-lg bg-blue-900 hover:bg-blue-800 focus:ring-blue-800">
+        <a href={link} target="_blank" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white rounded-lg bg-blue-900 hover:bg-blue-800 focus:ring-blue-800">
             Explore
             <svg className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
         </a>
@@ -159,7 +162,7 @@ function ExamLayout({ title, explain, url }) {
       </a>
 
       <p className="mb-3 font-normal text-gray-700"><i>{explain}</i></p>
-      <a href="#" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white rounded-lg bg-blue-900 hover:bg-blue-800 focus:ring-blue-800">
+      <a href={url} target="_blank" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white rounded-lg bg-blue-900 hover:bg-blue-800 focus:ring-blue-800">
             Explore
             <svg className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
         </a>
@@ -268,7 +271,7 @@ export default function Home({ products, definitions, exams }) {
                   
                   "
                   >
-                 Exams
+                 Exams & Cases
                </h2>
 
             </div>
@@ -280,7 +283,7 @@ export default function Home({ products, definitions, exams }) {
             key={sig.title}
             title={sig.title}
             explain={sig.explain}
-            url={sig.link}
+            url={sig.url}
           />    
         ))}
         </div>
