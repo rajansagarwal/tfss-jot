3696480ed324a9375ac89e9726f0aacd2c07b886
Rajan Agarwal <rajan.ag005@gmail.com>
2022-03-21 21:18:18 -0400

    target blanki

--- pages/index.js
+++ pages/index.js
@@ -114,7 +114,7 @@ function Product({ name, about, status, category, link }) {
 
         <p className="mb-3 font-normal text-gray-700"><i>{category}</i></p>
         <p className="mb-3 font-normal text-gray-700">{about}</p>
-        <a href={link} target="_blank" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white rounded-lg bg-blue-900 hover:bg-blue-800 focus:ring-blue-800">
+        <a href={link} target="_blank" rel="noopener" noreferrer className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white rounded-lg bg-blue-900 hover:bg-blue-800 focus:ring-blue-800">
             Explore
             <svg className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
         </a>
@@ -162,7 +162,7 @@ function ExamLayout({ title, explain, url }) {
       </a>
 
       <p className="mb-3 font-normal text-gray-700"><i>{explain}</i></p>
-      <a href={url} target="_blank" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white rounded-lg bg-blue-900 hover:bg-blue-800 focus:ring-blue-800">
+      <a href={url} target="_blank" rel="noopener" noreferrer className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white rounded-lg bg-blue-900 hover:bg-blue-800 focus:ring-blue-800">
             Explore
             <svg className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
         </a>
